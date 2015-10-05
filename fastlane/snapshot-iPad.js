70ac9a629e136f4f9cfd2b1854f28591ad531c04
Nitin Alabur <nalabur@optoro.com>
2015-10-05 15:01:34 -0400

    updated snapshot-iPad.js to run through similar actions for screenshots

--- fastlane/snapshot-iPad.js
+++ fastlane/snapshot-iPad.js
@@ -1,9 +1,21 @@
 #import "SnapshotHelper.js"
 
+// var target = UIATarget.localTarget();
+// var app = target.frontMostApp();
+// var window = app.mainWindow();
+
+
+// target.delay(3)
+// captureLocalizedScreenshot("0-LandingScreen")
+
 var target = UIATarget.localTarget();
-var app = target.frontMostApp();
-var window = app.mainWindow();
-
-
-target.delay(3)
-captureLocalizedScreenshot("0-LandingScreen")
+// target.scheme = TestIOSApp
+target.setDeviceOrientation(UIA_DEVICE_ORIENTATION_PORTRAIT);
+target.frontMostApp().mainWindow().buttons()["Button 1"].tap();
+captureLocalizedScreenshot('button1-tapped')
+target.frontMostApp().mainWindow().buttons()["Button 2"].tap();
+captureLocalizedScreenshot('button2-tapped')
+target.frontMostApp().mainWindow().buttons()["Button 3"].tap();
+captureLocalizedScreenshot('button3-tapped')
+target.frontMostApp().mainWindow().buttons()["Button 4"].tap();
+captureLocalizedScreenshot('button4-tapped')
